12ddab0cc45d943da8e150cc286903bbc8be0e60
Amiodas <amio.das.dip@gmail.com>
2023-06-27 22:28:47 +0100

    #profile data show

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Profile = () => {
-    return (
-        <div>
-            Profile
+  const { user, loading } = useContext(AuthContext);
+  console.log(user);
+  return (
+    <div className="min-h-screen my-16">
+    <h1 className="text-3xl font-bold text-center mb-10">User Profile</h1>
+      {!loading ? (
+        <div className="text-center">
+          <div className="card border rounded-lg w-48 p-2 mx-auto">
+            <img src={user?.photoURL} alt="" className="rounded-lg" />
+          </div>
+          <div className="mt-5">
+            <h2 className="text-xl font-bold">User: {user?.displayName}</h2>
+            <p className="font-bold">Email: {user?.email}</p>
+          </div>
         </div>
-    );
+      ) : (
+        <div className="flex justify-center py-16">
+          <span className="loading loading-spinner text-orange-600 loading-lg"></span>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
